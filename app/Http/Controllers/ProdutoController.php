a51628ab33822d3917d4cd759ac203df6f094779
vagrant <vagrant@homestead>
2017-01-26 13:56:48 +0000

    Capítulo 6 finalizado

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,27 @@
+<?php namespace estoque\Http\Controllers;
+
+use	Illuminate\Support\Facades\DB;
+use Request;
+
+class ProdutoController extends Controller{
+	
+	public	function lista(){
+		
+		$produtos = DB::select('select * from produtos');
+		
+		return view('listagem')->with('produtos', $produtos);	
+	}
+
+	public function mostra($id){
+		
+		$resposta = DB::select('select	*	from	produtos	where	id	=	?',[$id]);
+		
+		if(empty($resposta)){
+			return	"Esse produto não existe";
+		}
+
+		return	view('detalhes')->with('p',	$resposta[0]);
+
+	}
+
+}
